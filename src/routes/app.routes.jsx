ab26c85d104eb721dc4042b2379fae55b55a2583
Leonardo Paiva <leonardo.paiva@terceirizadoraizen.com>
2023-08-16 09:44:56 -0300

    fix route

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -5,17 +5,9 @@ import Details from '../pages/Details'
 import EditFood from '../pages/EditFood'
 import AddFood from '../pages/AddFood'
 
-import SignIn from '../pages/SignIn'
-import LogIn from '../pages/LogIn'
-
-
 export function AppRoutes(){
     return(
         <Routes>
-
-            <Route path="/" element={<LogIn/>}/> 
-            <Route path="/signin" element={<SignIn/>}/> 
-
             <Route path="/" element={<Menu/>}/>
             <Route path="/user/:id" element={<Details/>}/>
             <Route path="/admin/:id" element={<Details/>}/>
